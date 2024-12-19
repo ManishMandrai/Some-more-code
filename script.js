b0a9e7aa9e81cfb9fcbe00546b86cb2d75837282
ManishMandrai <manishkumarmandrai@gmail.com>
2024-12-19 15:48:37 +0530

    .

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
-// if (true) {
-//     var a = 10;
-// }
-// console.log(a);
+if (true) {
+    var a = 10;
+}
+console.log(a);
 
-// if (true) {
-//     let b = 20;
-//     console.log(b);
-// }
+if (true) {
+    let b = 20;
+    console.log(b);
+}
 
-// const c = 30;
-// console.log(c);
+const c = 30;
+console.log(c);
 
-// let num1 = 5;
-// let num2 = 300;
-// const sum = num1 + num2;
-// console.log("Sum is", sum);
+let num1 = 5;
+let num2 = 300;
+const sum = num1 + num2;
+console.log("Sum is", sum);
  
 
-// if (true) {
-//     let x = 50;
-//     var y = 100;
-//     console.log(x);
-// }
-// console.log(y);
+if (true) {
+    let x = 50;
+    var y = 100;
+    console.log(x);
+}
+console.log(y);
 
 
 
-// let count = 34;
+let count = 34;
 
-// function increment() {
-//     count++;
-//     console.log("Count", count);
-// }
-// function decrement(){
-//     count--;
-//     console.log("Count", count);
-// }
+function increment() {
+    count++;
+    console.log("Count", count);
+}
+function decrement(){
+    count--;
+    console.log("Count", count);
+}
 
-// increment();
-// decrement();
+increment();
+decrement();
 
 
 
-// let name = "Manish" 
-// if (true) {
-//     console.log("My Name is:", name);
-// }
+let name = "Manish" 
+if (true) {
+    console.log("My Name is:", name);
+}
 
 
 let age = 34;
@@ -57,3 +57,56 @@ console.log(typeof age);
 console.log(typeof isAdult);
 console.log(typeof hobbies);
 console.log(typeof person);
+
+
+let x  = 10,  y = 5
+console.log(x+y);
+console.log(x%y);
+x++
+console.log(x);
+
+let c = 10;
+c += 5
+console.log(c);
+
+let g = 25, f = "25";
+console.log(f == g);
+console.log(f === g);
+
+let x = 10, y = "10";
+console.log(x == y);  
+console.log(x === y); 
+
+
+let x = true, y = false;
+console.log(x && y);
+console.log(x || y);
+console.log(!x);
+ 
+
+let age = 23;
+let isAdult = age >= 25 ? "yes" : "no";
+console.log(isAdult);
+
+
+let x = true
+console.log(typeof x);
+
+
+let a = 10, b = 23;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+let age = 10;
+console.log(age >= 18);
+
+let isStudent = true, hasID = false;
+console.log((isStudent && hasID));
+
+
+let score = 32;
+let result = score >= 50 ? "pass" : "fail";
+console.log(result);
