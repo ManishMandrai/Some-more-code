6a2056c85895554771db6bb156b982f5d6340545
ManishMandrai <manishkumarmandrai@gmail.com>
2024-12-19 15:30:14 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+// if (true) {
+//     var a = 10;
+// }
+// console.log(a);
+
+// if (true) {
+//     let b = 20;
+//     console.log(b);
+// }
+
+// const c = 30;
+// console.log(c);
+
+// let num1 = 5;
+// let num2 = 300;
+// const sum = num1 + num2;
+// console.log("Sum is", sum);
+ 
+
+// if (true) {
+//     let x = 50;
+//     var y = 100;
+//     console.log(x);
+// }
+// console.log(y);
+
+
+
+// let count = 34;
+
+// function increment() {
+//     count++;
+//     console.log("Count", count);
+// }
+// function decrement(){
+//     count--;
+//     console.log("Count", count);
+// }
+
+// increment();
+// decrement();
+
+
+
+// let name = "Manish" 
+// if (true) {
+//     console.log("My Name is:", name);
+// }
+
+
+let age = 34;
+let isAdult = true;
+let hobbies = ["Reaing", "Coding", "Flirting"];
+let person = {name : "Manish", age:23};
+
+console.log(typeof age);
+console.log(typeof isAdult);
+console.log(typeof hobbies);
+console.log(typeof person);
